a6c06b687e72e646891c835f09a3b6141b399b93
trevor ngugi <ngugitrevor@gmail.com>
2019-11-11 00:34:33 +0300

    add js constructor and function of pizza toppings prices

---
+++ js/scripts.js
@@ -0,0 +1,16 @@
+function Topping(name,large,medium,small){
+    this.name=name;
+    this.large=large;
+    this.medium=medium;
+    this.small=small;}
+var marinatedChicken=new Topping("marinated chicken",200,100,50);
+var marinatedSteak=new Topping("marinated BBQ steak",200,100,50);
+var bacon=new Topping("bacon",160,80,40);
+var pineapple=new Topping("pineapple",120,60,30);
+var beefMushroom=new Topping("beef mushroom",160,80,40);
+var beefPepperoni=new Topping("beef pepperoni",160,80,40);
+var tomatoes=new Topping("tomatoes",120,60,30);
+var onions=new Topping("onions",120,60,30);
+var periperiChicken=new Topping("periperi chicken",200,100,50);
+var sweetcorn=new Topping("sweet corn",160,80,40);
+
